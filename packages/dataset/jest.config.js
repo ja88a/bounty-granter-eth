dc2661e5851f408db7a13c4ce7ad47d9b32345cb
ja88a <r0g3r@tuta.io>
2022-10-11 11:42:35 +0200

    chore(mocha and js-yaml integ tests)

--- packages/dataset/jest.config.js
+++ packages/dataset/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    transform: {
-      '^.+\\.ts?$': 'ts-jest',
-    },
-    testEnvironment: 'node',
-    testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    roots: ['<rootDir>/src'],
-  };
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  testRegex: './test/.*\\.(test|spec)?\\.(ts|ts)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/test'],
+};
